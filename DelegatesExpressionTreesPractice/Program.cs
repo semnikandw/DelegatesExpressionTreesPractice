72f62a24a3b34637f524ed1e8e2072f2380c74e3
Nikolai Semenov <n.semenov@atol.ru>
2018-05-19 15:20:12 +0300

    delegetes as param

--- DelegatesExpressionTreesPractice/Program.cs
+++ DelegatesExpressionTreesPractice/Program.cs
@@ -8,28 +8,31 @@ namespace DelegatesExpressionTreesPractice
 {
     class Program
     {
-        delegate int Operation(int x, int y);
+        delegate void GetMessage();
 
         static void Main(string[] args)
         {
-            // присваивание адреса метода через контруктор
-            Operation del = new Operation(Add); // делегат указывает на метод Add
-            int result = del.Invoke(4, 5);
-            Console.WriteLine(result);
-
-            del = Multiply; // теперь делегат указывает на метод Multiply, присваивание such us old
-            result = del.Invoke(4, 5);
-            Console.WriteLine(result);
-
-            Console.Read();
+            if (DateTime.Now.Hour < 12)
+            {
+                Show_Message(GoodMorning);
+            }
+            else
+            {
+                Show_Message(GoodEvening);
+            }
+            Console.ReadLine();
         }
-        private static int Add(int x, int y)
+        private static void Show_Message(GetMessage _del)
         {
-            return x + y;
+            _del.Invoke();
         }
-        private static int Multiply(int x, int y)
+        private static void GoodMorning()
         {
-            return x * y;
+            Console.WriteLine("Good Morning");
+        }
+        private static void GoodEvening()
+        {
+            Console.WriteLine("Good Evening");
         }
     }
 }
